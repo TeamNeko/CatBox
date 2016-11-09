ff37787e8f291ced3c5bef8bcde93a7ba778737e
Alexandre Kirouac <alexandre.kirouac@gmail.com>
2016-11-08 21:12:09 -0500

    Call API
    Détails boîte (contenu)

--- Frontend/src/org/teamneko/schrodinger/dao/InventoryDAO.java
+++ Frontend/src/org/teamneko/schrodinger/dao/InventoryDAO.java
@@ -1,8 +1,12 @@
 package org.teamneko.schrodinger.dao;
 
+import java.util.List;
+
+import org.teamneko.meowlib.dto.NamedProduct;
 import org.teamneko.meowlib.pojo.InventoryItem;
 
 public interface InventoryDAO {
 	public void addToInventory(InventoryItem item);
 	public void removeFromInventory(InventoryItem item);
+	public List<NamedProduct> getBoxContents(int idBox);
 }
