d12c4bb4a295412a0c7dbd96ca61c8c5d2f93b16
Tommy Savaria <UXTimeZone@gmail.com>
2016-11-11 13:16:38 -0500

    Plus d'expérimentations avec JavaFX

--- Backend/src/application/SampleController.java
+++ Backend/src/application/SampleController.java
@@ -1,5 +1,10 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class SampleController {
-	
+	@FXML protected void handleSubmitButtonAction(ActionEvent event) {
+		System.exit(0);
+	}
 }
