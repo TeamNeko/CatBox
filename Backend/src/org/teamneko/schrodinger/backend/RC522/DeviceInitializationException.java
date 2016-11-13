51817dbef8f0741a0695856839200fba584b3ba8
Tommy Savaria <UXTimeZone@gmail.com>
2016-11-13 15:14:59 -0500

    Début du refactor
    Formattage et réorganisation des éléments dans MFRC522.
    Déplacement de l'initialisation du SPI dans le constructeur.
    Création des classes d'exception Pi4JNotFound et DeviceInitialization.
    Le factory détecte Pi4J à l'avance lance une exception Pi4JNotFound quand Pi4J n'est pas disponible.
    Le constructeur de MFRC522 lance une DeviceInitializationException si erreur lors de l'initalisation du SPI.
    Le MFRC522 n'est construit que si celui-ci est fonctionnel.
    currentId est une variable membre de l'objet et non plus statique comme avant
    Fiou. Ca fait pas mal le tour. Et c'est pas fini.

---
+++ Backend/src/org/teamneko/schrodinger/backend/RC522/DeviceInitializationException.java
@@ -0,0 +1,26 @@
+package org.teamneko.schrodinger.backend.RC522;
+
+public class DeviceInitializationException extends Exception {
+	private static final long serialVersionUID = -1465790667080873043L;
+
+	public DeviceInitializationException() {
+		super();
+	}
+
+	public DeviceInitializationException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public DeviceInitializationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DeviceInitializationException(String message) {
+		super(message);
+	}
+
+	public DeviceInitializationException(Throwable cause) {
+		super(cause);
+	}
+}
