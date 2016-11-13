2631d5c9eb61805f246aeb1307da26f25d4e6056
Tommy Savaria <UXTimeZone@gmail.com>
2016-11-13 15:48:28 -0500

    Exemple de modification de Pane avec FXML et Controller

--- Backend/src/org/teamneko/schrodinger/backend/fx/Controller.java
+++ Backend/src/org/teamneko/schrodinger/backend/fx/Controller.java
@@ -1,9 +1,27 @@
 package org.teamneko.schrodinger.backend.fx;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.SplitPane;
 
 public class Controller {
+	private static ApplicationState state = ApplicationState.ReadingRFID;
+	
+	@FXML protected SplitPane mainPane;
+	
+	@FXML protected void modifier(ActionEvent event) throws IOException {
+		if(state == ApplicationState.ReadingRFID) {
+			state = ApplicationState.EditingBox;
+			mainPane.getItems().set(1, FXMLLoader.load(getClass().getResource("TablePane.fxml")));
+		} else if(state == ApplicationState.EditingBox) {
+			state = ApplicationState.ReadingRFID;
+			mainPane.getItems().set(1, FXMLLoader.load(getClass().getResource("AnimationPane.fxml")));
+		}
+	}
+	
 	@FXML protected void quitter(ActionEvent event) {
 		System.exit(0);
 	}
