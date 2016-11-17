42079e6696ed560dcdd9751bba929fce31ca0c86
Tommy Savaria <UXTimeZone@gmail.com>
2016-11-17 13:38:00 -0500

    MVC Model

--- Backend/src/org/teamneko/schrodinger/backend/fx/ApplicationLauncher.java
+++ Backend/src/org/teamneko/schrodinger/backend/fx/ApplicationLauncher.java
@@ -1,23 +1,23 @@
 package org.teamneko.schrodinger.backend.fx;
 	
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
-import javafx.fxml.FXMLLoader;
 
 
-public class Main extends Application {
+public class ApplicationLauncher extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
+		Context context = Context.getInstance();
 		try {
-			SplitPane root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-			Scene scene = new Scene(root,320,240);
+			Scene scene = new Scene(context.getMainWindow(),480,320);
+			Context.getInstance().setKeyboardHandler(new KeyboardHandler(scene));
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.setScene(scene);
+			//primaryStage.setFullScreen(true);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
