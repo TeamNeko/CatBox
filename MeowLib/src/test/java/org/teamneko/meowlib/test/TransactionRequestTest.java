aec56e5a5551804872442f9807a30533f2d3e69a
Tommy Savaria <UXTimeZone@gmail.com>
2016-11-18 16:41:52 -0500

    Changé les dossiers pour utiliser la convention Gradle

---
+++ MeowLib/src/test/java/org/teamneko/meowlib/test/TransactionRequestTest.java
@@ -0,0 +1,14 @@
+package org.teamneko.meowlib.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TransactionRequestTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
