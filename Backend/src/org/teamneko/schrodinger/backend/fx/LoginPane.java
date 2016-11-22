829c6d2906bd2b027de48aceff32f74e50f747d3
Twilightlink02 <Fellink12@gmail.com>
2016-11-22 17:19:20 -0500

    Partie Login Controller à finir

--- Backend/src/org/teamneko/schrodinger/backend/fx/LoginPane.java
+++ Backend/src/org/teamneko/schrodinger/backend/fx/LoginPane.java
@@ -1,4 +1,4 @@
-package org.teamneko.schrodinger.backend.fx;
+/*package org.teamneko.schrodinger.backend.fx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -21,3 +21,107 @@ public class LoginPane extends CustomAnchorPane {
 		 
 	 }
 }
+*/
+
+package org.teamneko.schrodinger.backend.fx;
+
+import org.teamneko.schrodinger.backend.RC522.MFRC522;
+import org.teamneko.schrodinger.backend.RC522.RFIDFactory;
+
+import com.sun.jersey.api.client.UniformInterfaceException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginPane extends CustomAnchorPane {
+	@FXML protected TextField rfidText;
+	 
+	public LoginPane() {
+		super();
+	}
+	
+	 @FXML protected void login(ActionEvent event) {
+		// Context.getInstance().login(rfidText.getText());
+		 if(Context.getInstance().login(rfidText.getText())){
+			 
+		 }
+		 else{
+			 
+		 }
+		 
+	 }
+
+
+	 
+	 
+	 
+	 
+	 
+	//main du Swing	
+		MFRC522 rc522;
+		//private main_window window;
+		
+		
+		public static void main(String[] args) {
+		//	main_window window = new main_window(); À FAIRE POUR FX (Alex l'a fait?)
+		//	UserAuthentification userAuth = new UserAuthentification(window);
+			MFRC522 rc522 = RFIDFactory.createMFRC522();
+			
+					try {
+					
+					boolean doSleep = userAuth.runAuthentification();  // À FAIRE POUR FX
+						if (doSleep){
+							Thread.sleep(5000);
+						}
+					}
+					catch (Exception e) { 
+						e.printStackTrace();
+						}
+		}
+	
+//Authentification (Login RFID)
+
+	  public void authentification(){
+		  initialize();
+	  
+	  }
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		//Mettre Labels, buttons, etc ici!!! Ou call interface quelque part d'autre?
+	};
+	
+	public boolean runAuthentification() throws InterruptedException{
+	
+					
+		
+		
+		String nullID = "0000000000";
+		
+		
+		while(!rc522.readID()){
+			try {
+				
+				if(rc522.currentID == nullID)
+				{
+					window.jLabel_Xrouge.setVisible(true);	//changer path to Xrouge
+				}
+			
+			} catch (UniformInterfaceException e) { //Exception
+				e.printStackTrace();
+			} 
+		}
+		window.jLabel_crochetvert.setVisible(true); //changer Path to crochetvert
+	return true;
+
+	};
+
+
+
+
+
+
+}
+
