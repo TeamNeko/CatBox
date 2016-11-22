eaf68c7959a25536f1f0cea88d5ab0018bbf84a3
Victor Fan <victorfan@hotmail.ca>
2016-11-21 21:03:10 -0500

    Fix nom de test
    Ajout de DTO dans le nom du test TransactioNRequestTest

---
+++ MeowLib/src/test/java/org/teamneko/meowlib/test/DtoTransactionRequestTest.java
@@ -0,0 +1,54 @@
+package org.teamneko.meowlib.test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.teamneko.meowlib.json.TransactionRequest;
+import org.teamneko.meowlib.json.TransactionRequest.Product;
+
+public class DtoTransactionRequestTest {
+	
+	private TransactionRequest testTransactionRequest;
+	private TransactionRequest.Product testTransactProduct;
+	private List<Product> productsModified;
+	
+	@Before 
+	public void initiateTestTransactionRequest() {
+		testTransactionRequest = new TransactionRequest();
+		testTransactProduct = new TransactionRequest.Product();
+		productsModified = new ArrayList();
+	}
+	
+	@Test
+	public void getSetTransactionRequestTest() 
+	{ 
+		assertEquals("",testTransactionRequest.getBox());
+		assertEquals(-1,testTransactionRequest.getUser());
+		assertTrue(testTransactionRequest.getProductsModified().isEmpty());
+		
+		testTransactProduct = new TransactionRequest.Product(1, 2);
+		assertEquals(1,testTransactProduct.getId()); 
+		assertEquals(2,testTransactProduct.getQuantity());
+		
+		testTransactProduct.setId(30);
+		testTransactProduct.setQuantity(50);
+		productsModified.add(testTransactProduct);
+		testTransactionRequest.setBox("123BE");
+		testTransactionRequest.setUser(1);
+		testTransactionRequest.setProductsModified(productsModified);
+		assertEquals("123BE",testTransactionRequest.getBox());
+		assertEquals(1,testTransactionRequest.getUser());
+		assertEquals(30,testTransactionRequest.getProductsModified().get(0).getId());
+		assertEquals(50,testTransactionRequest.getProductsModified().get(0).getQuantity());
+		
+		testTransactionRequest = new TransactionRequest(100,"TESTINIT",new ArrayList());
+		assertEquals("TESTINIT",testTransactionRequest.getBox());
+		assertEquals(100,testTransactionRequest.getUser());
+		assertTrue(testTransactionRequest.getProductsModified().isEmpty());
+	}
+
+}
