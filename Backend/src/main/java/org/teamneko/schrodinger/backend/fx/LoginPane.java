a4e78502b076c6b9a0e5a9567f9e5f7ccf0c3a8c
Victor Fan <victorfan@hotmail.ca>
2016-11-23 20:50:43 -0500

    Update LoginPane
    Ajout de l'ancien code swing en commentaire.

--- Backend/src/main/java/org/teamneko/schrodinger/backend/fx/LoginPane.java
+++ Backend/src/main/java/org/teamneko/schrodinger/backend/fx/LoginPane.java
@@ -42,4 +42,72 @@ public class LoginPane extends CustomAnchorPane implements RFIDCallback {
 		if(Context.getInstance().login(id) && thread != null)
 			thread.interrupt();
 	}
+	/*
+	@FXML protected TextField rfidText;
+	 
+	public LoginPane() {
+		super();
+	}
+	
+	 @FXML protected void login(ActionEvent event) {
+		// Context.getInstance().login(rfidText.getText());
+		 if(Context.getInstance().login(rfidText.getText())){
+			 
+		 }
+		 else{
+			 
+		 }
+		 
+	 }
+
+	//main du Swing	
+	MFRC522 rc522;
+	//private main_window window;
+	public static void main(String[] args) {
+	//	main_window window = new main_window(); À FAIRE POUR FX (Alex l'a fait?)
+	//	UserAuthentification userAuth = new UserAuthentification(window);
+		MFRC522 rc522 = DeviceFactory.createMFRC522();
+		
+		try {
+		
+		boolean doSleep = userAuth.runAuthentification();  // À FAIRE POUR FX
+			if (doSleep){
+				Thread.sleep(5000);
+			}
+		}
+		catch (Exception e) { 
+			e.printStackTrace();
+			}
+	}
+	
+//Authentification (Login RFID)
+
+	public void authentification(){
+	  initialize();
+  
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		//Mettre Labels, buttons, etc ici!!! Ou call interface quelque part d'autre?
+	};
+	
+	public boolean runAuthentification() throws InterruptedException{
+		String nullID = "0000000000";
+		while(!rc522.read()){
+			try {
+				
+				if(rc522.currentID == nullID)
+				{
+					//window.jLabel_Xrouge.setVisible(true);	//changer path to Xrouge
+				}
+			
+			} catch (UniformInterfaceException e) { //Exception
+				e.printStackTrace();
+			} 
+		}
+		//window.jLabel_crochetvert.setVisible(true); //changer Path to crochetvert
+	return true;
+	};
+	*/
 }
